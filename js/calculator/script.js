57edfc2363ad1c1654e1208266c545f16e40819a
KamaPulyaa <miraiaddams15@gmail.com>
2026-02-07 15:11:47 +0500

    edited

--- js/calculator/script.js
+++ js/calculator/script.js
@@ -2,4 +2,4 @@ var a = +prompt("Введите цифру").trim();
 var b = prompt("Введите знак (+ - * /)").trim();
 var c = +prompt("Введите цифру").trim();
 
-console.log(eval(a + b + c +));
+console.log(eval(a + b + c ));
